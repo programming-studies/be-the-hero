d7610282f7b23a936889635e16f44985196d08aa
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-03-25 17:46:30 -0300

    recurso de deletar cao (incident)

--- backend/src/controllers/IncidentsController.js
+++ backend/src/controllers/IncidentsController.js
@@ -9,5 +9,15 @@ module.exports = {
         const ong_id = request.headers.authorization;
         const [ id ] = await databaseConnection(tableName).insert({ title, description, value, ong_id });
         return response.json({ id });
+    },
+    delete: async (request, response) => {
+        const { id } = request.params;
+        const ong_id = request.headers.authorization;
+        const incident = await databaseConnection(tableName).select('ong_id').where('id', id).first();
+        if (incident.ong_id !== ong_id) {
+            return response.status(401).json({ error: 'Operation not permitted.' });
+        }
+        await databaseConnection(tableName).where('id', id).delete();
+        return response.status(204).send();
     }
 }
