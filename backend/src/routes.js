182ef94ffd64690082f02cedc0097c85f695531c
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-03-25 14:43:28 -0300

    cadastrar ong

---
+++ backend/src/routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const crypto = require("crypto");
+const databaseConnection = require("./database/connection");
+
+const routes = express.Router();
+
+routes.post('/ongs', async (request, response) => {
+    const { name, email, whatsapp, city, uf } = request.body;
+    const id = crypto.randomBytes(4).toString('HEX'); // gera 4 bytes e converte em string hexadecimal
+    await databaseConnection('ongs').insert({ id, name, email, whatsapp, city, uf });
+    return response.json({ id });
+})
+
+module.exports = routes;
