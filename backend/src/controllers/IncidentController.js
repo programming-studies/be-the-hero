b1cfa292fcf8775edf747e992d71a5e6a840676e
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-03-25 19:20:40 -0300

    trazendo iformações da Ong ao listar Casos da ong

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -5,8 +5,21 @@ module.exports = {
     index: async (request, response) => {
         const { page = 1 } = request.query;
         const [ count ] = await databaseConnection(tableName).count();
+        const ongTableName = 'ongs';
         response.header('X-Total-Count', count['count(*)']);
-        response.json(await databaseConnection(tableName).select('*').limit(5).offset((page - 1) * 5))
+        const incidents = await databaseConnection(tableName)
+            .select([
+                `${tableName}.*`,
+                `${ongTableName}.name`,
+                `${ongTableName}.email`,
+                `${ongTableName}.whatsapp`,
+                `${ongTableName}.city`,
+                `${ongTableName}.uf`,
+            ])
+            .limit(5)
+            .offset((page - 1) * 5)
+            .join(ongTableName, `${ongTableName}.id`, '=', `${tableName}.ong_id`);
+        response.json(incidents);
     },
     list: async (request, response) => {
         const { id } = request.params;
