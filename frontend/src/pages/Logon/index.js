b71beb800c1c4db0ca38a0e406e993f705600b2c
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-03-29 19:47:32 -0300

    tela de logon no frontend

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
 
-export default function index() {
+import './style.css';
+
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
+
+export default function index () {
     return (
-        <div>
-            Logon
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+                <form>
+                    <h1>Faça seu logon</h1>
+                    <input type="text" placeholder="Digite seu ID"/>
+                    <button type="submit" className="button">Entrar</button>
+                    <a href="">
+                        <FiLogIn size={16} color="#e02041"/>
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+            <img src={heroesImg} alt="heroes.png"/>
         </div>
     )
 }
