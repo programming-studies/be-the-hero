ef24c5b2171e1356819b3224abe05a0c12efba64
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-03-30 12:05:58 -0300

    concluido css da tela de listar casos

--- frontend/src/pages/ListCases/index.js
+++ frontend/src/pages/ListCases/index.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiPower, FiTrash2 } from "react-icons/fi";
+
+import "./style.css";
+
+import logoImg from "../../assets/logo.svg";
 
 export default function ListCases() {
-    return (
-        <div>
-            List Cases
-        </div>
-    )
+  return (
+    <div className="list-cases-container">
+      <header>
+        <img src={logoImg} alt="Be The Hero" />
+        <span>Bem vinda, APAD</span>
+        <Link className="button" to="/incidents/new">
+          Cadastrar novo caso
+        </Link>
+        <button>
+          <FiPower size={18} color="#e02041" />
+        </button>
+      </header>
+
+      <h1>Casos cadastrados</h1>
+
+      <ul>
+        <li>
+          <strong>CASO:</strong>
+          <p>Cadelinha atropelada</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Cadelinha atropelada</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button>
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
