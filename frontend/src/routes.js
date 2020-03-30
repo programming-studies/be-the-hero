ef7f052bf6efc2a2281d1a330ca422796ccee2ea
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-03-29 23:58:43 -0300

    criacao da página de cadastro

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import LogonPage from './pages/Logon';
+import RegisterPage from './pages/Register';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" component={LogonPage}/>
+                <Route path="/" exact component={LogonPage}/>
+                <Route path="/register" component={RegisterPage}/>
             </Switch>
         </BrowserRouter>
     )
