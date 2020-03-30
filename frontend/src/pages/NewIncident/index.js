560018078cb6b33d47873abdeba50bc2b1417e4a
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-03-30 16:50:15 -0300

    concluido css da tela de cadastrar novo caso

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
 
-import './style.css'
+import "./style.css";
+
+import logoImg from "../../assets/logo.svg";
 
 export default function NewIncident() {
-    return (
-        <div>
-            New Incident
-        </div>
-    )
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero" />
+          <h1>Cadastrar novo caso</h1>
+          <p>
+            Descreva o caso detalhadamente para encontrar um héroi para resolver
+            isso.
+          </p>
+          <Link className="back-link" to="/list-cases">
+            <FiArrowLeft size={16} color="#e02041" />
+            Voltar para home
+          </Link>
+        </section>
+        <form action="">
+          <input type="text" placeholder="Título do caso" />
+          <textarea cols="30" rows="10" placeholder="Descrição"></textarea>
+          <input type="text" placeholder="Valor em reais" />
+          <div className="input-group">
+            <Link className="button" to="/">Cancelar</Link>
+            <button type="submit" className="button">
+              Cadastrar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
