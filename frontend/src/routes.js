1474fdd286dbff0b234307b4cd477e58029a3b6e
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-03-30 12:11:37 -0300

    criação da página decriar caso para ong

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import ListCasesPage from "./pages/ListCases";
 import LogonPage from "./pages/Logon";
+import NewIncidentPage from "./pages/NewIncident";
 import RegisterPage from "./pages/Register";
 
 export default function Routes() {
@@ -12,6 +13,7 @@ export default function Routes() {
         <Route path="/" exact component={LogonPage} />
         <Route path="/register" component={RegisterPage} />
         <Route path="/list-cases" exact component={ListCasesPage} />
+        <Route path="/incidents/new" exact component={NewIncidentPage} />
       </Switch>
     </BrowserRouter>
   );
