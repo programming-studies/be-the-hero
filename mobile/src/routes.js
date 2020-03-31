7c912eee84c51171fa4a1cd1e447f4aeaef43606
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-03-31 16:19:21 -0300

    adicionando rotas e página de incidents e details no mobile

---
+++ mobile/src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import IncidentsPage from "./pages/Incidents";
+import DetailsPage from "./pages/Detail";
+
+const AppStack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator>
+        <AppStack.Screen name="Incidents" component={IncidentsPage} />
+        <AppStack.Screen name="Details" component={DetailsPage} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}
