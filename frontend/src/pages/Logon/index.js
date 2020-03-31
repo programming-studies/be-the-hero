95edd4c26179349b6a062f36132e90e7036f319d
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-03-30 23:10:43 -0300

    feito logon chamando a api

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,23 +1,38 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
+import api from '../../services/api';
 
 import "./style.css";
 
 import logoImg from "../../assets/logo.svg";
 import heroesImg from "../../assets/heroes.png";
 
-export default function index() {
+export default function Logon() {
+  const [id, setId] = useState("");
+
+  const history = useHistory();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await api.post("sessions", { id });
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", response.data.name);
+      history.push("/list-cases");
+    } catch (error) {
+      alert("Erro, por favor tente novamente mais tarde.");
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
-          <input type="text" placeholder="Digite seu ID" />
-          <button type="submit" className="button">
-            Entrar
-          </button>
+          <input type="text" placeholder="Digite seu ID" value={id} onChange={event => setId(event.target.value)} />
+          <button type="submit" className="button">Entrar</button>
           <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#e02041" />
             Não tenho cadastro
