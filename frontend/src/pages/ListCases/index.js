903088e47d5ab6a5efd7591a3ee20680ebd7abde
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-03-30 23:27:19 -0300

    após logar, vai para tela de listar casos e exibe o nome da ong

--- frontend/src/pages/ListCases/index.js
+++ frontend/src/pages/ListCases/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FiPower, FiTrash2 } from "react-icons/fi";
 
@@ -7,11 +7,17 @@ import "./style.css";
 import logoImg from "../../assets/logo.svg";
 
 export default function ListCases() {
+  const [ongName, setOngName] = useState("");
+
+  useEffect(() => {
+    setOngName(localStorage.getItem("ongName"));
+  }, [])
+
   return (
     <div className="list-cases-container">
       <header>
         <img src={logoImg} alt="Be The Hero" />
-        <span>Bem vinda, APAD</span>
+        <span>Bem vindo(a), {ongName}</span>
         <Link className="button" to="/incidents/new">
           Cadastrar novo caso
         </Link>
