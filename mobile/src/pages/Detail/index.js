938598173c8784bc95d702845f6c37d199e2d84f
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-04-01 22:11:33 -0300

    prepara mensagem e abre o whatsapp para enviar a mesma a partir do app mobile

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
 import * as MailComposer from 'expo-mail-composer';
@@ -25,7 +25,7 @@ export default function Detail() {
   }
 
   function sendWhatsapp() {
-    navigation.goBack();
+    Linking.openURL(`whatsapp://send?phone=+5583988202968&text=${message}`)
   }
 
   return (
@@ -59,7 +59,7 @@ export default function Detail() {
         <Text style={styles.heroDescription}>Entre em contato</Text>
 
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
             <Text style={styles.actionText}>Whatsapp</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.action} onPress={sendMail}>
