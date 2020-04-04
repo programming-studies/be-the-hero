1e620a981c959c64157da23048c75dbd4e7ee8a3
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-04-04 11:22:46 -0300

    adicionada validação de campos na rota de criar ong

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { celebrate, Segments, Joi } = require("celebrate");
 
 const IncidentsController = require("./controllers/IncidentController");
 const OngController = require("./controllers/OngController");
@@ -9,7 +10,15 @@ const routes = express.Router();
 routes.post('/sessions', SessionController.store);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.store);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.store);
 
 routes.get('/ongs/:id/incidents', IncidentsController.list);
 
