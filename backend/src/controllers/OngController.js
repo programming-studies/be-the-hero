9fb3aa3d2760f3dcc6906208f9dc20d6cd67ecc3
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-04-04 15:46:44 -0300

    extraindo o gerador de id para um arquivo a parte  para reaproveitar

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,13 +1,12 @@
-const crypto = require("crypto");
 const databaseConnection = require("../database/connection");
-const tableName = 'ongs';
+const generateUniqueId = require("../utils/generateUniqueId");
+const tableName = "ongs";
 
 module.exports = {
-    index: async(request, response) => response.json(await databaseConnection(tableName).select('*')),
-    store: async (request, response) => {
-        const { name, email, whatsapp, city, uf } = request.body;
-        const id = crypto.randomBytes(4).toString('HEX'); // gera 4 bytes e converte em string hexadecimal
-        await databaseConnection(tableName).insert({ id, name, email, whatsapp, city, uf });
-        return response.json({ id });
-    }
+  index: async(request, response) => response.json(await databaseConnection(tableName).select('*')),
+  store: async (request, response) => {
+    const { id = generateUniqueId(), name, email, whatsapp, city, uf } = request.body;
+    await databaseConnection(tableName).insert({ id, name, email, whatsapp, city, uf });
+    return response.json({ id });
+  }
 }
